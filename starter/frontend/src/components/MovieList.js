ea1fcbb433ff152f4671ecedaaeec1722edc8b4f
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-06-10 23:13:49 +0700

    update

---
+++ starter/frontend/src/components/MovieList.js
@@ -0,0 +1,29 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {url_movies} from '../route';
+
+export default function MovieList({ onMovieClick }) {
+  const [movies, setMovies] = useState(null);
+
+  useEffect(() => {
+    axios.get(url_movies).then((response) => {
+      setMovies(response.data.movies);
+    });
+  }, null);
+
+  return (
+    <>
+        <ul>
+            {
+                movies != null ? movies.map((item) => {
+                    <li className='movieItem' key={item.id} onClick={() => onMovieClick(item.id)}>
+                        <a href='javascript:void(0)'>{item.title} - <span>{item.description}</span></a>
+                    </li>
+                }) : <li className='movieItem' key='0'>
+                    Empty List
+                </li>
+            }
+        </ul>
+    </>
+  );
+}
---
+++ starter/frontend/src/route.js
@@ -0,0 +1,3 @@
+const host = process.env.BACKEND_API;
+//Get Data from API (back-end)
+export const url_movies = `${host}/movies`;
