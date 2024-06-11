021a768fa13a80a3c26894fe59aa6ffe971cc8d1
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-06-11 19:30:37 +0700

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -2,23 +2,23 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
 export default function MovieList({ onMovieClick }) {
-  const [responseData, setMovies] = useState(null);
+  const [responseData, setMovies] = useState([]);
 
   useEffect(() => {
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data);
     });
-  }, null);
+  }, []);
 
   return (
     <>
       <ul>
-        {responseData != null ? (
-          responseData.movies.map((item) => {
+        {responseData && responseData.movies && responseData.movies.length > 0 ? (
+          responseData.movies.map((item) => (
             <li className="movieItem" key={item.id} onClick={() => onMovieClick(item.id)}>
               <a href="javascript:void(0)">{item.title}</a> - <span>{item.description}</span>
-            </li>;
-          })
+            </li>
+          ))
         ) : (
           <li className="movieItem" key="0">
             Empty List
