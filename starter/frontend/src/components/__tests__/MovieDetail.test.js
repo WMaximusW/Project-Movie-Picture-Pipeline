40bf2e4ee356b97cc4e19c5ef0463326116aec33
WMaximusW <wnguyenquoctrung1993w@gmail.com>
2024-06-11 07:41:14 +0700

    Update MovieDetail.test.js

--- starter/frontend/src/components/__tests__/MovieDetail.test.js
+++ starter/frontend/src/components/__tests__/MovieDetail.test.js
@@ -4,40 +4,29 @@ import '@testing-library/jest-dom/extend-expect'; // for better assertions
 import axios from 'axios';
 import MovieDetail from '../MovieDetail';
 
+const mockMovieData = {
+  movie: {
+    id: 1,
+    title: 'Movie 1',
+    description: 'Description 1',
+  },
+};
+
+jest.mock('axios', () => {
+  const expectedResponse = JSON.stringify(mockMovieData);
+  return () => new Promise((resolve) => resolve(expectedResponse));
+});
+
 describe('On click Detail', () => {
   it('Get data of the movie, return data', async () => {
     const mockMovieId = 1;
-    const mockMovieData = {
-      movie: {
-        id: mockMovieId,
-        title: 'Movie 1',
-        description: 'Description 1',
-      },
-    };
-
-    jest.mock('axios', () => {
-      const expectedResponse = JSON.stringify(mockMovieData);
-      return () => new Promise((resolve) => resolve(expectedResponse));
-    });
 
     render(<MovieDetail movieId={mockMovieId} />);
+
+    axios.get.mockResolvedValueOnce({ data: mockMovieData });
 
     // Wait for the movie details to be loaded
     expect(await screen.findByText('Movie 1')).toBeInTheDocument();
     expect(await screen.findByText('Description 1')).toBeInTheDocument();
-  });
-
-  it('Data is empty', async () => {
-    const mockMovieId = 1;
-
-    jest.mock('axios', () => {
-      const expectedResponse = JSON.stringify(null);
-      return () => new Promise((resolve) => resolve(expectedResponse));
-    });
-
-    render(<MovieDetail movieId={mockMovieId} />);
-
-    // Wait for the movie details to be loaded
-    expect(await screen.findByText('Data not found')).toBeInTheDocument();
   });
 });
